c5110907de3b84b9e563d1226eeddb321c8307ed
Urfan Musayev <83137111+urfan03@users.noreply.github.com>
2024-03-01 14:14:33 +0400

    holberton

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,37 +1,38 @@
 #!/usr/bin/python3
 
-'''This script determines if all boxes can be opened in a scenario with boxes containing keys to other boxes.'''
+
+'''This script determines if all boxes can be opened.'''
 
 def canUnlockAll(boxes):
     """
     Determines if all the boxes can be opened.
-    
+
     Args:
         boxes (list of list of int): List of boxes, each containing keys.
-        
+
     Returns:
         bool: True if all boxes can be opened, else False.
     """
     if not boxes:
         return False
-    
+
     # Initialize a set to keep track of visited boxes
     visited = set()
     # Initialize a stack with the first box
     stack = [0]
-    
+
     while stack:
         # Pop a box from the stack
         current_box = stack.pop()
         # Mark the box as visited
         visited.add(current_box)
-        
+
         # Check all keys in the current box
         for key in boxes[current_box]:
-            # If the key opens a box that hasn't been visited yet, add it to the stack
+            # If the key opens a box that hasn't been visited yet
             if key < len(boxes) and key not in visited:
                 stack.append(key)
-    
+
     # If all boxes have been visited, return True
     return len(visited) == len(boxes)
 
