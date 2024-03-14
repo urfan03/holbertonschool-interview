838ec7f68d486f18aa52472c6a34133789de4db6
urfan03 <urfanmusayev2003@gmail.com>
2024-03-14 22:28:45 +0400

    holberton

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -1,33 +1,26 @@
-
-#include <stdio.h>
-#include <stdlib.h>
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - creates a binary tree node
- * @parent: pointer to the parent node of the node to create
- * @value: value to put in the new node
- * Return: pointer to the new node, or NULL on failure
+ * binary_tree_node - Create a binary node
+ * @parent: node parent
+ * @value: Value of the node
+ * Return: the new node
  */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-binary_tree_t *new;
+	binary_tree_t *new_node;
 
-new = malloc(sizeof(binary_tree_t));
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
 
-if (new == NULL)
-{
-return (NULL);
-}
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
 
-new->n = value;
-new->left = NULL;
-new->right = NULL;
-new->parent = NULL;
-
-if (parent != NULL)
-new->parent = parent;
-
-return (new);
+	return (new_node);
 }
--- heap_insert/1-heap_insert.c
+++ heap_insert/1-heap_insert.c
@@ -1,190 +1,82 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "binary_trees.h"
 
 /**
- * heap_insert - function that inserts a value in Max Binary Heap
- * @value: value to be inserted
- * @root: tree root
- * Return: pointer to the created node, or NULL on failure.
+ * binary_tree_size - function that return the size of a tree
+ * @tree: tree to check
+ * Return: size of tree
  */
 
-void correct_heap(heap_t **root) {
-    if ((*root)->left != NULL && (*root)->right != NULL) {
-        correct_heap(&(*root)->left);
-        correct_heap(&(*root)->right);
-    } else if ((*root)->left != NULL && (*root)->right == NULL && (*root)->left->left != NULL) {
-        heap_t *new = (*root)->left;
-        if (new->n > new->left->n) {
-            (*root)->right = new;
-            (*root)->right->parent = *root;
-            (*root)->left = new->left;
-            (*root)->left->parent = *root;
-            new->left = NULL;
-        } else {
-            (*root)->right = new->left;
-            (*root)->right->parent = *root;
-            new->left = NULL;
-        }
-    } else if ((*root)->right != NULL && (*root)->left == NULL && (*root)->right->left != NULL) {
-        heap_t *new = (*root)->right;
-        if (new->n < new->left->n) {
-            (*root)->left = new;
-            (*root)->left->parent = *root;
-            (*root)->right = new->left;
-            (*root)->right->parent = *root;
-            new->left = NULL;
-        } else {
-            (*root)->left = new->left;
-            (*root)->left->parent = *root;
-            new->left = NULL;
-        }
-    }
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t size = 0, left = 0, right = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		left = binary_tree_size(tree->left);
+		right = binary_tree_size(tree->right);
+		size = left + right + 1;
+	}
+
+	return (size);
 }
 
-int height(heap_t *root) {
-    int left, right;
+heap_t *insert_ordered(binary_tree_t *tree, int value, int idx, int n_leaf)
+{
+	heap_t *node;
 
-    if (root == NULL) {
-        return (0);
-    }
+	if (tree == NULL)
+	{
+		return (NULL);
+	}
 
-    left = height(root->left);
-    right = height(root->right);
+	if (idx + 1 == n_leaf / 2)
+	{
+		if (n_leaf & 1)
+		{
+			tree->right = binary_tree_node(tree, value);
+			return (tree->right);
+		}
+		else
+		{
+			tree->left = binary_tree_node(tree, value);
+			return (tree->left);
+		}
+	}
 
-    if (left > right) {
-        return (left + 1);
-    } else {
-        return (right + 1);
-    }
+	node = insert_ordered(tree->left, value, 2 * idx + 1, n_leaf);
+
+	if (node != 0)
+	{
+		return (node);
+	}
+	else
+	{
+		return (insert_ordered(tree->right, value, 2 * idx + 2, n_leaf));
+	}
 }
 
-int left_is_full(heap_t *root, int h) {
-    int count = 0;
-    heap_t *temp = root;
+heap_t *heap_insert(heap_t **root, int value)
+{
+	heap_t *node;
 
-    if (temp == NULL) {
-        temp = temp->left;
-    }
+	if (*root == NULL)
+	{
+		*root = binary_tree_node(NULL, value);
+		return (*root);
+	}
 
-    while (temp->left != NULL && temp->right != NULL) {
-        count++;
-        temp = temp->left;
-    }
+	node = insert_ordered(*root, value, 0, binary_tree_size(*root) + 1);
 
-    if (count == h - 1) {
-        return (1);
-    } else {
-        return (0);
-    }
-}
+	while (node->parent && node->n > node->parent->n)
+	{
+		node->n = node->parent->n;
+		node->parent->n = value;
+		node = node->parent;
+	}
 
-int right_is_full(heap_t *root, int h) {
-    int count = 0;
-    heap_t *temp = root;
-
-    if (temp == NULL) {
-        temp = temp->right;
-    }
-
-    while (temp->left != NULL && temp->right != NULL) {
-        count++;
-        temp = temp->right;
-    }
-
-    if (count == h - 1) {
-        return (1);
-    } else {
-        return (0);
-    }
-}
-
-heap_t *heap_insert(heap_t **root, int value) {
-    heap_t *new;
-    int left_is_ful, right_is_ful;
-
-    if (*root == NULL) {
-        *root = malloc(sizeof(heap_t));
-        if (*root == NULL) {
-            return NULL;
-        }
-        (*root)->n = value;
-        (*root)->left = NULL;
-        (*root)->right = NULL;
-        (*root)->parent = NULL;
-        return *root;
-    }
-
-    new = malloc(sizeof(heap_t));
-
-    if (new == NULL) {
-        return (NULL);
-    }
-
-    new->n = value;
-    new->left = NULL;
-    new->right = NULL;
-    new->parent = NULL;
-
-    if ((*root)->n > value && (*root)->left == NULL)
-    {
-        new->parent = *root;
-        (*root)->left = new;
-        return (new);
-    }
-    else if ((*root)->n > value && (*root)->right == NULL)
-    {
-        new->parent = *root;
-        (*root)->right = new;
-        return (new);
-    }
-    else if (((*root)->n < value)) {
-        int left_is_ful = left_is_full(*root, height(*root));
-        int right_is_ful = right_is_full(*root, height(*root));
-
-
-        if ((left_is_ful == 1 && right_is_ful == 1) || (left_is_ful == 0 && right_is_ful == 1) || (left_is_ful == 0 && right_is_ful == 0)) {
-            if ((*root)->parent == NULL && (*root)->left != NULL) {
-                new->parent = (*root)->parent;
-                new->right = (*root)->right;
-                new->left = (*root);
-                (*root)->right = NULL;
-                *root = new;
-            } else {
-                new->parent = (*root)->parent;
-                new->left = *root;
-                (*root)->parent = new;
-                *root = new;
-            }
-        }
-        else if (left_is_ful == 1 && right_is_ful == 0) {
-            if ((*root)->parent == NULL && (*root)->right != NULL) {
-                new->parent = (*root)->parent;
-                new->left = (*root)->left;
-                new->right = (*root);
-                (*root)->left = NULL;
-                *root = new;
-            } else {
-                new->parent = (*root)->parent;
-                new->right = *root;
-                (*root)->parent = new;
-                *root = new;
-            }
-        }
-
-        correct_heap(root);
-
-        return (new);
-    }
-
-    left_is_ful = left_is_full(*root, height(*root));
-    right_is_ful = right_is_full(*root, height(*root));
-
-    if ((left_is_ful == 1 && right_is_ful == 1) || (left_is_ful == 0 && right_is_ful == 1) || (left_is_ful == 0 && right_is_ful == 0)) {
-        heap_insert(&(*root)->left, value);
-    } else {
-        heap_insert(&(*root)->right, value);
-    }
-
-    return (new);
+	return (node);
 }
