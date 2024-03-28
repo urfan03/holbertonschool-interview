57bb6f40812dd5d70c60f0801ce9c11d8f40ca67
Urfan Musayev <83137111+urfan03@users.noreply.github.com>
2024-03-28 18:18:07 +0400

    holberton

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -1,13 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
-
 #include "sandpiles.h"
 
 /**
  * stable - Checks if sandpile is stable
  * @grid: 3x3 grid to be printed
  *
- * Return: Is sandpile stable or not(1,0)
+ * Return: Is sandpile stable or not (1,0)
  */
 int stable(int grid[3][3])
 {
@@ -20,10 +19,10 @@ int stable(int grid[3][3])
 
 	return (1);
 }
+
 /**
  * print_grid - Print 3x3 grid
  * @grid: 3x3 grid
- *
  */
 static void print_grid(int grid[3][3])
 {
@@ -42,7 +41,7 @@ static void print_grid(int grid[3][3])
 }
 
 /**
- * sandpiles_sum - Summs two sandpiles into grid1
+ * sandpiles_sum - Sums two sandpiles into grid1
  * @grid1: Left 3x3 grid
  * @grid2: Right 3x3 grid
  */
@@ -52,10 +51,8 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 
 	for (i = 0; i < 3; i++)
 		for (j = 0; j < 3; j++)
-		{
 			grid1[i][j] += grid2[i][j];
 			grid2[i][j] = 0;
-		}
 
 	if (stable(grid1))
 		return;
@@ -77,13 +74,11 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 					if (j < 2)
 						grid2[i][j + 1] += 1;
 				}
+
 		for (i = 0; i < 3; i++)
-		{
 			for (j = 0; j < 3; j++)
-			{
 				grid1[i][j] += grid2[i][j];
 				grid2[i][j] = 0;
-			}
-		}
+
 	} while (!stable(grid1));
 }
