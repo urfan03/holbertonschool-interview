020538e29bd14e0a626f85c00ff77d7ec171fa4d
Urfan Musayev <83137111+urfan03@users.noreply.github.com>
2024-03-28 17:53:09 +0400

    holberton

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -8,18 +8,18 @@
  */
 void print_grid2(int grid[3][3])
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (j != 0)
-                printf(" ");
-            printf("%d", grid[i][j]);
-        }
-        printf("\n");
-    }
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (j != 0)
+				printf(" ");
+			printf("%d", grid[i][j]);
+		}
+		printf("\n");
+	}
 }
 
 /**
@@ -31,13 +31,13 @@ void print_grid2(int grid[3][3])
  */
 void add(int grid1[3][3], int grid2[3][3])
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-            grid1[i][j] += grid2[i][j];
-    }
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+			grid1[i][j] += grid2[i][j];
+	}
 }
 
 /**
@@ -49,31 +49,31 @@ void add(int grid1[3][3], int grid2[3][3])
  */
 void tupple(int grid[3][3], int tmp[3][3])
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-            tmp[i][j] = 0;
-    }
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (grid[i][j] > 3)
-            {
-                tmp[i][j] -= 4;
-                if (i + 1 < 3)
-                    tmp[i + 1][j] += 1;
-                if (j + 1 < 3)
-                    tmp[i][j + 1] += 1;
-                if (i - 1 >= 0)
-                    tmp[i - 1][j] += 1;
-                if (j - 1 >= 0)
-                    tmp[i][j - 1] += 1;
-            }
-        }
-    }
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+			tmp[i][j] = 0;
+	}
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (grid[i][j] > 3)
+			{
+				tmp[i][j] -= 4;
+				if (i + 1 < 3)
+					tmp[i + 1][j] += 1;
+				if (j + 1 < 3)
+					tmp[i][j + 1] += 1;
+				if (i - 1 >= 0)
+					tmp[i - 1][j] += 1;
+				if (j - 1 >= 0)
+					tmp[i][j - 1] += 1;
+			}
+		}
+	}
 }
 
 /**
@@ -84,17 +84,17 @@ void tupple(int grid[3][3], int tmp[3][3])
  */
 int stable(int grid[3][3])
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (grid[i][j] > 3)
-                return (0);
-        }
-    }
-    return (1);
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (grid[i][j] > 3)
+				return (0);
+		}
+	}
+	return (1);
 }
 
 /**
@@ -106,14 +106,14 @@ int stable(int grid[3][3])
  */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-    int tmp[3][3];
+	int tmp[3][3];
 
-    add(grid1, grid2);
-    while (!stable(grid1))
-    {
-        printf("=\n");
-        print_grid2(grid1);
-        tupple(grid1, tmp);
-        add(grid1, tmp);
-    }
+	add(grid1, grid2);
+	while (!stable(grid1))
+	{
+		printf("=\n");
+		print_grid2(grid1);
+		tupple(grid1, tmp);
+		add(grid1, tmp);
+	}
 }
