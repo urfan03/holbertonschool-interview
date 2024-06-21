c4000bb8982635b47007bd76454d74dce487c403
urfan03 <urfanmusayev2003@gmail.com>
2024-06-21 14:45:44 +0400

    done

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -1,25 +1,28 @@
 #!/usr/bin/node
 
 const request = require('request');
-const arg = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/${arg}`;
 
-const charNames = (characters, i = 0) => {
-  if (i === characters.length) return;
-  request(characters[i], (error, response, body) => {
-    if (error) throw error;
-    // Convert a string of characters JSON to a JavaScript object and print it
-    console.log(JSON.parse(body).name);
-    // Call recursively the function charNames and increment it to pass to the next character
-    charNames(characters, i + 1);
-  });
-};
+const filmNum = process.argv[2] + '/';
+const filmURL = 'https://swapi-api.hbtn.io/api/films/';
 
-// Request to the API to recover movie information
-request(url, function (error, response, body) {
-  if (error) throw error;
-  // Extract URL of characters from the body JSON response and store it in a var char
-  const char = JSON.parse(body).characters;
-  // Call the function charNames to print the characters
-  charNames(char);
+// Makes API request, sets async to allow await promise
+request(filmURL + filmNum, async function (err, res, body) {
+  if (err) return console.error(err);
+
+  // find URLs of each character in the film as a list obj
+  const charURLList = JSON.parse(body).characters;
+
+  // Use URL list to character pages to make new requests
+  // await queues requests until they resolve in order
+  for (const charURL of charURLList) {
+    await new Promise(function (resolve, reject) {
+      request(charURL, function (err, res, body) {
+        if (err) return console.error(err);
+
+        // finds each character name and prints in URL order
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
+    });
+  }
 });
