f9f1e952b8a5dd4a48124bfd6df35d76d9375304
urfan03 <urfanmusayev2003@gmail.com>
2024-09-26 10:34:13 +0400

    done

--- island_perimeter/0-island_perimeter.py
+++ island_perimeter/0-island_perimeter.py
@@ -1,26 +1,75 @@
 #!/usr/bin/python3
-
-""" Function to find perimiter of an island """
+"""
+Defines function to calculate the perimeter of an island
+represented by a list of list of ints
+"""
 
 
 def island_perimeter(grid):
     """
-    Input: List of Lists
-    Returns: Perimeter of the island
+    Calculates the perimeter of an island represented by a list of list of ints
+
+    parameters:
+        grid [list of list of ints]: represents the island & surrounding water
+            * 0s represent water & 1s represent land
+            * Each cell is a sqaure with side length of 1
+            * Cells are connected horizontally/vertically (not diagonally)
+            * Grid is rectangular, with width and height not exceeding 100
+            * The grid is completely surrounded by water
+            * Islands do not have "lakes" (water completely inside the island)
+            * There is only one island or nothing
+
+    returns:
+        the perimeter of the island
     """
-    count = 0
-    row = len(grid)
-    col = len(grid[0]) if row else 0
+    error_msg = "grid must be a list of lists of ints representing an island"
+    if type(grid) is not list:
+        raise TypeError(error_msg)
+    for row in grid:
+        if type(row) is not list:
+            raise TypeError(error_msg)
+        for column in row:
+            if type(column) is not int:
+                raise TypeError(error_msg)
+            if column is not 0 and column is not 1:
+                raise ValueError(
+                    "grid must contain only 0s & 1s representing water & land")
+    perimeter = 0
+    for row in range(len(grid)):
+        for column in range(len(grid[row])):
+            if grid[row][column] == 1:
+                perimeter += is_edge(grid, row, column)
+    return perimeter
 
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
 
-            idx = [(i - 1, j), (i, j - 1), (i, j + 1), (i + 1, j)]
-            check = [1 if k[0] in range(row) and k[1] in range(col) else 0
-                     for k in idx]
+def is_edge(grid, row, column):
+    """
+    Determines if the given cell is on the edge of the island
 
-            if grid[i][j]:
-                count += sum([1 if not r or not grid[k[0]][k[1]] else 0
-                              for r, k in zip(check, idx)])
+    parameters:
+        grid [list of lists of ints]: represents the island
+        row [int]: the index of row for the current cell
+        column [int]: the index of column for the current cell
 
-    return (count)
+    returns:
+        1-4, if the given cell is on an edge, the number of edges
+        0, if the cell of land is in the interior of the island
+    """
+    edge_count = 0
+    if row == 0:
+        edge_count += 1
+    if row == (len(grid) - 1):
+        edge_count += 1
+    if row != 0 and grid[row - 1][column] == 0:
+        edge_count += 1
+    if row != (len(grid) - 1) and grid[row + 1][column] == 0:
+        edge_count += 1
+    if column == 0:
+        edge_count += 1
+    if column == (len(grid[row]) - 1):
+        edge_count += 1
+    if column != 0 and grid[row][column - 1] == 0:
+        edge_count += 1
+    if column != (len(grid[row]) - 1) and grid[row][column + 1] == 0:
+        edge_count += 1
+    return edge_count
