8672128ff2b9f4097bc4307ba7026cb13ad932f8
urfan03 <urfanmusayev2003@gmail.com>
2024-10-22 21:32:01 +0400

    done

--- island_perimeter/0-island_perimeter.py
+++ island_perimeter/0-island_perimeter.py
@@ -1,66 +1,28 @@
 #!/usr/bin/python3
-""" Island Perimeter mandatory """
-
-
-def test0(n):
-    """
-    *******************************
-    ******test if n is 0 or 1******
-    *******************************
-    @n: number of the grid
-    Returns: 0 if n is 1 or 1 if n is 0
-    """
-    if (n == 0):
-        return 1
-    return 0
+"""
+Island Perimeter
+"""
 
 
 def island_perimeter(grid):
     """
-    ***********************************************
-    ******returns the perimeter of the island******
-    ***************described in grid***************
-    ***********************************************
-    @grid: is a list of list of integers:
-                *** 0 represents water
-                *** 1 represents land
-                *** Each cell is square, with a side
-                    length of 1
-                *** Cells are connected horizontally/vertically
-                    (not diagonally).
-                *** grid is rectangular, with its width and
-                    height not exceeding 100
-            The grid is completely surrounded by water
-            There is only one island (or nothing).
-            The island doesn’t have “lakes” (water inside that isn’t
-                connected to the water surrounding the island).
+    grid is a list of list of integers:
+    0 represents water
+    1 represents land
+    Each cell is square, with a side length of 1
+    Cells are connected horizontally/vertically (not diagonally).
+    grid is rectangular, with its width and height not exceeding 100
     """
-    row = len(grid)
-    col = len(grid[0])
-    assert (1 <= row and col <= 100), "length must be between 1 an 100"
-
-    p = 0
-    for i in range(row):
-        for j in range(col):
-            assert (grid[i][j] == 0) or (grid[i][j] == 1),\
-                                        "grid numbers must be 0 or 1"
-            if grid[i][j] == 1:
-                if i-1 < 0:
-                    p += 1
-                else:
-                    p += test0(grid[i-1][j])
-                if j-1 < 0:
-                    p += 1
-                else:
-                    p += test0(grid[i][j-1])
-
-                try:
-                    p += test0(grid[i+1][j])
-                except IndexError:
-                    p += 1
-                try:
-                    p += test0(grid[i][j+1])
-                except IndexError:
-                    p += 1
-
-    return p
+    perimeter = 0
+    for x in range(len(grid)):
+        for y in range(len(grid[x])):
+            if grid[x][y] == 1:
+                if x == 0 or grid[x - 1][y] == 0:
+                    perimeter += 1
+                if y == 0 or grid[x][y - 1] == 0:
+                    perimeter += 1
+                if x == len(grid) - 1 or grid[x + 1][y] == 0:
+                    perimeter += 1
+                if y == len(grid[x]) - 1 or grid[x][y + 1] == 0:
+                    perimeter += 1
+    return (perimeter)
